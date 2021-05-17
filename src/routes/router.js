7f1e86c2eed05ee443e5e07f4b97675511f08892
João Mota <47133204+jppm99@users.noreply.github.com>
2021-05-17 17:38:13 +0100

    added html response? maybe

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
 
-router.use('/', (req, res) => res.status(200));
+router.use('/', (req, res) => res.sendFile('../test_html.html', {root: __dirname }));
 
 module.exports = router;
