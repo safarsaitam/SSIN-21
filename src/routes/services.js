86a3fd5b28351d5227e732cf48ed6ec309603045
safarsaitam <jrffmatias@gmail.com>
2021-05-23 17:34:22 +0100

    Add route for nRoot on backend

--- src/routes/services.js
+++ src/routes/services.js
@@ -4,6 +4,7 @@ const serviceController = require('../controllers/service-controller');
 
 router.post('/sqrt', serviceController.squareRoot);
 router.post('/cbrt', serviceController.cubicRoot);
+router.post('/nrt', serviceController.nRoot);
 
 module.exports = router;
 
